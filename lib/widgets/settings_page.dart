e6ea7835baf1231f829d4ce7c757c49bbf2c5048
xnetai <xnet8080@gmail.com>
2025-02-01 17:48:45 +0200

    custom calendar with a menu

---
+++ lib/widgets/settings_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: Center(
+        child: Text('Settings Page'),
+      ),
+    );
+  }
+}
